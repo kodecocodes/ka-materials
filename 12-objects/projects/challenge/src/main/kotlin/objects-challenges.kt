252e79dd3bd97390a186c2fe41cf11f0889b13b8
Victoria Gonda <v.m.gonda@gmail.com>
2021-01-24 15:48:12 -0500

    Regenerate chapter 12 projects

--- 12-objects/projects/challenge/src/main/kotlin/objects-challenges.kt
+++ 12-objects/projects/challenge/src/main/kotlin/objects-challenges.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Razeware LLC
+ * Copyright (c) 2021 Razeware LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 
 /*
 
