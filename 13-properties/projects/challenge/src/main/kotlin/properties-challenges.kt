954fd63609074096f7ecbf1302656c6a3e78e6e7
Victoria Gonda <v.m.gonda@gmail.com>
2021-01-24 20:50:42 -0500

    Regenerate ch 13 projects

--- 13-properties/projects/challenge/src/main/kotlin/properties-challenges.kt
+++ 13-properties/projects/challenge/src/main/kotlin/properties-challenges.kt
@@ -1,22 +1,22 @@
 /*
- * Copyright (c) 2019 Razeware LLC
- * 
+ * Copyright (c) 2021 Razeware LLC
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
  * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * merger, publication, distribution, sublicensing, creation of derivative works,
  * or sale is expressly withheld.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -33,7 +33,7 @@ import kotlin.properties.Delegates
 class IceCream {
   var name: String = ""
   val ingredients: ArrayList<String> by lazy {
-     arrayListOf<String>()
+    arrayListOf<String>()
   }
 }
 
@@ -42,7 +42,7 @@ class Car(val make: String, val color: String, val tank: FuelTank = FuelTank())
 class FuelTank {
   var lowFuel = true
   var level: Double by Delegates.observable(0.0) {
-    _, _, new ->
+      _, _, new ->
     lowFuel = new < 0.1
   }
 }
