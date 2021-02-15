75b5ed96e003f239911c98439bf6c40855a9b2da
Victoria Gonda <v.m.gonda@gmail.com>
2021-02-15 16:42:20 -0500

    Chapter 19 FPE

--- 19-kotlin-java-interoperability/projects/final/src/main/java/User.java
+++ 19-kotlin-java-interoperability/projects/final/src/main/java/User.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Razeware LLC
+ * Copyright (c) 2021 Razeware LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -70,7 +70,9 @@ public class User {
 
   @Override
   public String toString() {
-    return UserExtensions.getFullName(this) + " - Addresses: " + addresses.size() + "\n" + allAddresses();
+    return UserExtensions.getFullName(this) +
+            " - Addresses: " + addresses.size() + "\n" +
+            allAddresses();
   }
 
   public String allAddresses() {
